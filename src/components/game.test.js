3e7c7577741e29c6c31227e52975f99735a09589
Alfredo <atakori@outlook.com>
2017-12-18 00:50:28 -0500

    added game.js tests

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -22,4 +22,29 @@ describe('<Game />', () => {
 		expect(wrapper.state('correctAnswer')).toBeGreaterThanOrEqual(0);
 		expect(wrapper.state('correctAnswer')).toBeLessThanOrEqual(100);
 	})
+	it('can make guesses', () => {
+		const wrapper= shallow(<Game/>)
+		wrapper.setState({
+			correctAnswer:86
+		})
+		wrapper.instance().makeGuess(33);
+		expect(wrapper.state('guesses')).toEqual([33]);
+		expect(wrapper.state('feedback')).toEqual('You\'re Ice Cold...')
+	})
+	it('can generate Aural Updates', () => {
+		const wrapper= shallow(<Game />);
+		wrapper.setState({
+			correctAnswer: 90
+		})
+		wrapper.instance().makeGuess(25);
+		wrapper.instance().makeGuess(39);
+		wrapper.instance().makeGuess(88);
+
+		wrapper.instance().generateAuralUpdate();
+
+		expect(wrapper.state('auralStatus')).toEqual(`Here's the status of the game right now: You're Hot! You've made 3 guesses. In order of most- to least-recent, they are: 88, 39, 25`);
+
+
+	})
+
 })
