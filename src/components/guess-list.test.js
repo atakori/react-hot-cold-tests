0957bb32ce5d11db792f9cb0e4d7995475324722
Alfredo <atakori@outlook.com>
2017-12-17 22:39:59 -0500

    added guess-list tests

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -4,13 +4,17 @@ import {shallow} from 'enzyme';
 import GuessList from './guess-list'
 
 describe('<GuessList />', () => {
-	let list= [];
-	beforeAll(() => {
-		for (let i=0; i<10; i++) {
-			list.push({i})
-		}
-	})
+	let list= [1,2,3,4];
 	it('renders without crashing', () => {
 		shallow(<GuessList guesses= {list} />);
+	})
+	it('renders a list of guesses', () => {
+		let list = [1,2,3,4];
+		const wrapper = shallow(<GuessList guesses= {list}/>)
+		const items = wrapper.find('li');
+		expect(items.length).toEqual(list.length);
+		list.forEach((value, index) => {
+			expect(items.at(index).text()).toEqual(value.toString());
+		})
 	})
 })
