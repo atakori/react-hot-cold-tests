d3ae4cac5d698c4494acb2e7662ff63ec5b3d23b
Alfredo <atakori@outlook.com>
2017-12-17 23:23:42 -0500

    added reset game test for game.js

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -7,4 +7,19 @@ describe('<Game />', () => {
 	it('renders without crashing', () => {
 		shallow(<Game />);
 	})
+	it('can start a new game', () => {
+		const wrapper= shallow(<Game/>)
+		wrapper.setState({
+			guesses: [5,6,7,8],
+      		feedback: 'Great!',
+     		auralStatus: 'None',
+      		correctAnswer: -12
+		})
+		wrapper.instance().restartGame();
+		expect(wrapper.state('guesses')).toEqual([])
+		expect(wrapper.state('feedback')).toEqual('Make your guess!');
+		expect(wrapper.state('auralStatus')).toEqual('');
+		expect(wrapper.state('correctAnswer')).toBeGreaterThanOrEqual(0);
+		expect(wrapper.state('correctAnswer')).toBeLessThanOrEqual(100);
+	})
 })
