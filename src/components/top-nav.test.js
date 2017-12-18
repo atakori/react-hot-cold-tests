ec7aeb36bcc09c59402cb831c0d45160fb354c54
Alfredo <atakori@outlook.com>
2017-12-17 22:51:06 -0500

    added callback tests for nav

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -7,4 +7,25 @@ describe('<TopNav />', () => {
 	it('renders without crashing', () => {
 		shallow(<TopNav/>);
 	})
+
+	it ('should call newGame() when it is clicked', () => {
+		const callback= jest.fn();
+		//fake function to be called
+		const wrapper= shallow(<TopNav onRestartGame= {callback}/>)
+		const link= wrapper.find('.new');
+		link.simulate('click', {
+			preventDefault() {}
+		});
+		expect(callback).toHaveBeenCalled();
+	})
+
+	it('should call on onGenerateAuralUpdate() when clicked', () => {
+		const callback= jest.fn();
+		const wrapper= shallow(<TopNav onGenerateAuralUpdate= {callback}/>);
+		const link = wrapper.find('.status-link');
+		link.simulate('click', {
+			preventDefault() {}
+		});
+		expect(callback).toHaveBeenCalled();
+	})
 })
