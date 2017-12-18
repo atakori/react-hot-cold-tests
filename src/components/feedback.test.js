86d5623b4084db20ce2d8fed01d69c038d687deb
Alfredo <atakori@outlook.com>
2017-12-17 22:08:16 -0500

    added tests for feedback.js

--- src/components/feedback.test.js
+++ src/components/feedback.test.js
@@ -7,4 +7,17 @@ describe('<Feedback />', () => {
 	it('renders without crashing', () => {
 		shallow(<Feedback guessCount= '1' />);
 	})
+	it('renders the feedback message', () => {
+		const key= '1';
+		const feedback_message= 'Hotter';
+		const guessAgain_message= 'guess again';
+		const wrapper = shallow(<Feedback feedback= {feedback_message} guessAgain= {guessAgain_message}/>);
+		expect(wrapper.contains(<h2 
+      key={key}
+      id="feedback"
+      role="status"
+      aria-live="assertive"
+      aria-atomic="true"
+    >{feedback_message} {guessAgain_message}</h2>));
+	})
 })
